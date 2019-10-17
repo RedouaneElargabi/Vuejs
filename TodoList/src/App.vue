17f32bfc159d8ea52c104ac26dc797eafc213e4e
RedouaneElargabi <56687207+RedouaneElargabi@users.noreply.github.com>
2019-10-17 17:51:11 +0200

    Add files via upload

---
+++ TodoList/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+
+        <!-- On importe la props de QuoteGrid avec un bind et on fais passer le tableau quotes-->
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Cliquer sur une citation pour l'effacer !</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+    export default {
+        data: function () {
+            return {
+                quotes: [                             // Représente le tableau de citation
+                    'Cliquez sur moi pour m\'effacer :('
+                ],
+                maxQuotes: 10                        // Représente le nombre de citation max
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Veuillez effacer quelques citations');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
+    }
+</script>
+
+<style>
+</style>
