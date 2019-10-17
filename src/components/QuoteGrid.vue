fbcbb8b9e25746d20f012af1b6df3f1b7f38eb22
RedouaneElargabi <56687207+RedouaneElargabi@users.noreply.github.com>
2019-10-17 17:12:59 +0200

    Add files via upload

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <!-- l'attribut native permet de rendre le composant cliquable -->
+        <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';   // On importe les citations
+
+    export default {
+        props: ['quotes'],
+
+        components: {
+            appQuote: Quote
+        },
+
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleted', index);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
+
+
+<!-- Ce component représente l'ensemble des citations -->
